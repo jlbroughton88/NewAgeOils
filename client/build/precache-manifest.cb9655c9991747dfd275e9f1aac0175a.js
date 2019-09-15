9d338990d4fa5906a2b56b6b1a5c5fddd2bf1e79
jlb1999 <JacobBroughton@JacobBroughtonsMacbook.local>
2019-09-15 17:51:36 -0400

    removed builded from client .gitignore

---
+++ client/build/precache-manifest.cb9655c9991747dfd275e9f1aac0175a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4b4674a739e84999e0fbce3935e871",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54b6b3a864cfb61ff91",
+    "url": "/static/css/main.f3ac28b4.chunk.css"
+  },
+  {
+    "revision": "aa71b6878726d3daa991",
+    "url": "/static/js/2.b8bd08fa.chunk.js"
+  },
+  {
+    "revision": "f54b6b3a864cfb61ff91",
+    "url": "/static/js/main.cf01fa22.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
